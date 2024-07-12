bdcf1c5e897dd01b73e732917f2e3f37c22fe41a
Puso90 <juwishbrands@gmail.com>
2024-07-12 06:34:14 +0200

    added startWith Method

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
+    
+    /*  Man! I searched far and wide for this startWith Method
+        I wanted to use a really easy and straight forward method 
+        that will not complicate code for me */
+
+// Probably easy, but I really searched! 
+    if (input.startsWith('pet_')) {
+        result = true;
+    } else {
+        result = false;
+    }
+    console.log()
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
             document.getElementById('result').innerText = result;
 }
+console.log(result);
+
 
 
